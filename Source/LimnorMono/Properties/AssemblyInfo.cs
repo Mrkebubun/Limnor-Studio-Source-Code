afb8a5418840790e4c97e45b12767b7f88228c59
Bob Limnor <info@limnor.com>
2016-08-21 15:41:09 -0700

    Add files via upload

--- Source/LimnorMono/Properties/AssemblyInfo.cs
+++ Source/LimnorMono/Properties/AssemblyInfo.cs
@@ -1,1 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Limnor Studio 5.6")]
+[assembly: AssemblyDescription("Limnor Studio is a generic-purpose visual codeless programming system with patented technologies. All kinds of software can be developed without learning and using textural computer languages.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd.")]
+[assembly: AssemblyProduct("Limnor Studio 5.6")]
+[assembly: AssemblyCopyright("Copyright © Longflow Enterprises Ltd 2014")]
+[assembly: AssemblyTrademark("Limnor is a registered trademark.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("946d1e04-8ca9-4365-82d5-ea7922a11c27")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("5.6.*")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
