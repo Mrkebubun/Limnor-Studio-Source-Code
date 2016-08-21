d967d8088bd9e0edba93b3c2c5013f356321ef6c
Bob Limnor <info@limnor.com>
2016-08-20 22:13:48 -0700

    Add files via upload

---
+++ Source/ConnectionManager/FormConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Author:	Bob Limnor (info@limnor.com)
+ * Project: Limnor Studio
+ * Item:	Database connection manager
+ * License: GNU General Public License v3.0
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LimnorDatabase;
+
+namespace ConnectionManager
+{
+	public partial class FormConnectionManager : DlgConnectionManager
+	{
+		public FormConnectionManager()
+		{
+			InitializeComponent();
+			HideOKCancelButtons();
+		}
+	}
+}
---
+++ Source/ConnectionManager/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Author:	Bob Limnor (info@limnor.com)
+ * Project: Limnor Studio
+ * Item:	Database connection manager
+ * License: GNU General Public License v3.0
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace ConnectionManager
+{
+	static class Program
+	{
+		[DllImportAttribute("user32.dll")]
+		static extern bool SetForegroundWindow(IntPtr hWnd);
+		static Mutex mutex;
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			bool b;
+			mutex = new Mutex(true, "69E7EC26-8FF7-422d-B153-EE6D3EDB685E", out b);
+			if (b || mutex != null)
+			{
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(new FormConnectionManager());
+			}
+			else
+			{
+				System.Diagnostics.Process p = System.Diagnostics.Process.GetCurrentProcess();
+				System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcessesByName(p.ProcessName);
+				for (int i = 0; (i < ps.Length); i++
+				)
+				{
+					if ((((ps[i].Id != p.Id)
+								&& (string.Compare(ps[i].MainModule.FileName, p.MainModule.FileName, System.StringComparison.OrdinalIgnoreCase) == 0))
+								&& (p.MainWindowHandle != System.IntPtr.Zero)))
+					{
+						SetForegroundWindow(ps[i].MainWindowHandle);
+						break;
+					}
+				}
+				System.Windows.Forms.MessageBox.Show("The Connection Manager is already running");
+				return;
+			}
+		}
+	}
+}
