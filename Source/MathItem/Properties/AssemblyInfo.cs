7a238e3c096582de4ea078d51fd4dfc48a33dabb
Bob Limnor <info@limnor.com>
2016-08-21 16:23:37 -0700

    Add files via upload

--- Source/MathItem/Properties/AssemblyInfo.cs
+++ Source/MathItem/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Math Expression Programming Library")]
+[assembly: AssemblyDescription("Provide property and method references in math expressions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd")]
+[assembly: AssemblyProduct("Limnor Codeless Visual Programming System")]
+[assembly: AssemblyCopyright("Copyright © Longflow 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1ef9806-c03c-4415-8e0b-edc2914d561b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
