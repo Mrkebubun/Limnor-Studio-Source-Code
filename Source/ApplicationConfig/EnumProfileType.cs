1c1e7c1c3fae2fecd435fedaf17185949a0ffd46
Bob Limnor <info@limnor.com>
2016-08-20 21:35:10 -0700

    Create EnumProfileType.cs

---
+++ Source/ApplicationConfig/EnumProfileType.cs
@@ -0,0 +1,20 @@
+/*
+ * Author:	Bob Limnor (info@limnor.com)
+ * Project: Limnor Studio
+ * Item:	Application Configuration component
+ * License: GNU General Public License v3.0
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Limnor.Application
+{
+	public enum EnumProfileType
+	{
+		Default = 0,
+		Named = 1,
+		User
+	}
+}
