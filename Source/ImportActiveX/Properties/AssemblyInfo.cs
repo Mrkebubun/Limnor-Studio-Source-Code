135f36a4553b07aeccf326187aeaf35a3f055ec5
Bob Limnor <info@limnor.com>
2016-08-21 11:55:47 -0700

    Add files via upload

--- Source/ImportActiveX/Properties/AssemblyInfo.cs
+++ Source/ImportActiveX/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Import ActiveX")]
+[assembly: AssemblyDescription("Import ActiveX components into a Limnor Studio project")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd")]
+[assembly: AssemblyProduct("Limnor Studio")]
+[assembly: AssemblyCopyright("Copyright © Longflow Enterprises Ltd 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0cdee289-c1ab-404e-82f2-eddd5301530c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
