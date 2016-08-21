ac4623683859d39b42fbc542be74496e752b6eb0
Bob Limnor <info@limnor.com>
2016-08-21 15:49:17 -0700

    Add files via upload

--- Source/LimnorWebBrowser/Properties/AssemblyInfo.cs
+++ Source/LimnorWebBrowser/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WebBrowser Control")]
+[assembly: AssemblyDescription("Web browser control derived from .Net WebBrowser, providing additional functionality")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd")]
+[assembly: AssemblyProduct("Limnor Codeless Visual Programming System")]
+[assembly: AssemblyCopyright("Copyright © Longflow 2010")]
+[assembly: AssemblyTrademark("Limnor")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("391a8e42-a756-4013-bc8c-cd2bd7a7a50c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
