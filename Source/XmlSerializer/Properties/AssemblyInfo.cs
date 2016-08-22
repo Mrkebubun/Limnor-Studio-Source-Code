6021fb4828010bd374bc2be040f9f31caba7a005
Bob Limnor <info@limnor.com>
2016-08-21 21:40:22 -0700

    Add files via upload

--- Source/XmlSerializer/Properties/AssemblyInfo.cs
+++ Source/XmlSerializer/Properties/AssemblyInfo.cs
@@ -1,1 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xml Serialization Library")]
+[assembly: AssemblyDescription("Xml Serialization Utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd")]
+[assembly: AssemblyProduct("Limnor Codeless Visual Programming System")]
+[assembly: AssemblyCopyright("Copyright © Longflow 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5520374b-0477-4396-9b00-db9a294b0501")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
