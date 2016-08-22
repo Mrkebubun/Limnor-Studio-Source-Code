b2814180659f2a8a69bb21cc904ab5eda1264b6c
Bob Limnor <info@limnor.com>
2016-08-22 12:30:48 -0700

    Add files via upload

---
+++ Source/LimnorCompiler/webApp/jm.cmd
@@ -0,0 +1,2 @@
+jsMinify <json_n_db.js >json_min.js "json2.js 2011-02-23 Public Domain. No warranty expressed or implied. Use at your own risk. This file has been superceded by http://www.JSON.org/json2.js See http://www.JSON.org/js.html"
+jsMinify <modal.js >modal_min.js "menu.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
