ed72dcf3e9155c7a2788469eb8d847d933472b16
Bob Limnor <info@limnor.com>
2016-08-22 12:31:56 -0700

    Add files via upload

---
+++ Source/WebBuilder/WebApp/jm2.cmd
@@ -0,0 +1,7 @@
+jsMinify <treeview.js >treeview_min.js "treeview.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <Filebrowser.js >Filebrowser_min.js "Filebrowser.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <limnorgooglemaps.js >limnorgooglemaps_min.js "limnorgooglemaps.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <limnorUtilString.js >limnorUtilString_min.js "limnorUtilityString.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <menu.js >menu_min.js "menu.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <htmleditor.js >htmleditor_min.js "htmleditor.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
+jsMinify <..\..\LimnorCompiler\WebApp\jsonDataBind.js >jsonDataBind_min.js "jsonDataBind.js 2012-01-09 Copyright Longflow Enterprises Ltd. http://www.limnor.com"
