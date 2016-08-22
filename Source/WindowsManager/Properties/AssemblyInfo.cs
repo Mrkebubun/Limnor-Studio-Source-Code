41f6f87bb5ee94f97cea89735f6578b38c0e56a1
Bob Limnor <info@limnor.com>
2016-08-21 21:23:49 -0700

    Add files via upload

--- Source/WindowsManager/Properties/AssemblyInfo.cs
+++ Source/WindowsManager/Properties/AssemblyInfo.cs
@@ -1,1 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using VPL;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Windows Management Utility Library")]
+[assembly: AssemblyDescription("Windows Management Utility in visual programming (http://www.limnor.com)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Longflow Enterprises Ltd")]
+[assembly: AssemblyProduct("Limnor Codeless Visual Programming System")]
+[assembly: AssemblyCopyright("Copyright © Longflow 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//[assembly: SupportFileAttribute("WinManUtil.Dll;WinUtil.Dll")]
+[assembly: SupportFileAttribute("NativeWindowsUtility.Dll;NativeWindowsUtility64.Dll")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91c7ea09-92e3-4678-9c4c-fa24c4ede9bf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("5.6.1.759")]
