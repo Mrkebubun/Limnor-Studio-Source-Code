b01997331dee07b451af157d0af18ec2e9652392
Bob Limnor <info@limnor.com>
2016-08-22 12:23:23 -0700

    Add files via upload

---
+++ Source/ExitKiosk/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ 
+ * Author:	Flora Lee (info@limnor.com)
+ * Project: Limnor Studio
+ * Item:	Kiosk Utility
+ * License: GNU General Public License v3.0
+ 
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LimnorKiosk;
+
+namespace ExitKiosk
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args != null && args.Length > 0)
+			{
+				if (string.Compare(args[0], "start", StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					LKiosk.Enterkiosk();
+				}
+				else if (string.Compare(args[0], "stop", StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					LKiosk.Exitkiosk();
+				}
+				else
+				{
+					Console.WriteLine("Usage:");
+					Console.WriteLine("kioskMode [start|stop]");
+					Console.WriteLine();
+					Console.WriteLine("If no argument is given then stop is used.");
+				}
+			}
+			else
+			{
+				LKiosk.Exitkiosk();
+			}
+		}
+	}
+}
